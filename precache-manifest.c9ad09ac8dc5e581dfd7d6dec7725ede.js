e9b53c6c7b71878f9fdbfb268d8dbac3bca3a4e7
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2019-09-23 15:51:25 +0530

    Updates

---
+++ precache-manifest.c9ad09ac8dc5e581dfd7d6dec7725ede.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b285891746c101a551a595c0b409ca9e",
+    "url": "/AOE-WEB/index.html"
+  },
+  {
+    "revision": "41e0000404e42776ecd4",
+    "url": "/AOE-WEB/static/js/2.c8444031.chunk.js"
+  },
+  {
+    "revision": "de58747ad6550345e7ff",
+    "url": "/AOE-WEB/static/js/main.cdb72aaf.chunk.js"
+  },
+  {
+    "revision": "50d84ee47d39b231aaba",
+    "url": "/AOE-WEB/static/js/runtime~main.5a82abd5.js"
+  }
+]);
